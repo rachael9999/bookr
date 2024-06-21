3adce034086d26fffe23280ce6842a11ef62ddd0
account@acipiexport.com account@acipiexport.com <mrq@seas.upenn.edu>
2024-06-21 20:09:27 +0100

    token refresh

--- easyevent/src/main/java/com/spring2go/easyevent/mapper/UserEntityMapper.java
+++
@@ -1,9 +0,0 @@
-package com.spring2go.easyevent.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.spring2go.easyevent.entity.UserEntity;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserEntityMapper extends BaseMapper<UserEntity> {
-}
--- easyevent/src/main/java/com/spring2go/easyevent/type/UserInput.java
+++
@@ -1,9 +0,0 @@
-package com.spring2go.easyevent.type;
-
-import lombok.Data;
-
-@Data
-public class UserInput {
-    private String email;
-    private String password;
-}
